eaa23b57894913b6becdb197f5382eff93de4081
MOLOCH-dev <sabnisanushree@gmail.com>
2025-04-18 12:35:38 -0400

    Load one image at a time

--- dataset_creation/OccupancyGridDataLoader.py
+++ dataset_creation/OccupancyGridDataLoader.py
@@ -8,79 +8,105 @@ import cairosvg
 import torchvision.transforms as transforms
 
 class OccupancyGridDataset(Dataset):
-    def __init__(self, root_dir, transform=None, grid_transform=None):
-        self.occupancy_dir = os.path.join(root_dir, 'occupancy')
-        self.views_dir = os.path.join(root_dir, 'svgs')
+    def __init__(self, occupancy_dir, svg_dir, transform=None, grid_transform=None):
+        self.occupancy_dir = occupancy_dir
+        self.svg_dir = svg_dir
         self.transform = transform
         self.grid_transform = grid_transform
 
-        # Base names from .npy files
-        self.sample_ids = [
-            os.path.splitext(f)[0]
-            for f in os.listdir(self.occupancy_dir)
-            if f.endswith('.npy')
-        ]
-        self.sample_ids.sort()
+        self.samples = []
 
-        self.view_suffixes = ['front', 'right', 'top']
+        # Create a mapping from occupancy base name to full path
+        occ_map = {
+            os.path.splitext(f)[0]: os.path.join(occupancy_dir, f)
+            for f in os.listdir(occupancy_dir)
+            if f.endswith('.npy')
+        }
+
+        # Go through SVGs and associate with occupancy
+        for svg_file in os.listdir(svg_dir):
+            if not svg_file.endswith('.svg'):
+                continue
+
+            # Parse base name and view direction
+            parts = os.path.splitext(svg_file)[0].rsplit('_', 1)
+            if len(parts) != 2:
+                continue
+            base_name, view_type = parts
+
+            if base_name in occ_map:
+                self.samples.append({
+                    'occ_path': occ_map[base_name],
+                    'svg_path': os.path.join(svg_dir, svg_file),
+                    'base_name': base_name,
+                    'view_type': view_type
+                })
+
+        self.samples.sort(key=lambda x: (x['base_name'], x['view_type']))
 
     def __len__(self):
-        return len(self.sample_ids)
+        return len(self.samples)
 
     def _load_svg_as_image(self, path):
-        """Converts an SVG file to a PIL Image."""
+        """Convert SVG file to PIL Image."""
         png_bytes = cairosvg.svg2png(url=path)
         image = Image.open(io.BytesIO(png_bytes)).convert("RGB")
         return image
 
     def __getitem__(self, idx):
-        sample_id = self.sample_ids[idx]
+        sample = self.samples[idx]
 
         # Load occupancy grid
-        grid_path = os.path.join(self.occupancy_dir, f"{sample_id}.npy")
-        grid = np.load(grid_path)
+        grid = np.load(sample['occ_path'])
         if self.grid_transform:
             grid = self.grid_transform(grid)
         else:
             grid = torch.tensor(grid, dtype=torch.float32)
 
-        # Load 3 views
-        views = []
-        for view_type in self.view_suffixes:
-            view_path = os.path.join(self.views_dir, f"{sample_id}_{view_type}.svg")
-            view = self._load_svg_as_image(view_path)
-            if self.transform:
-                view = self.transform(view)
-            else:
-                view = transforms.ToTensor()(view)
-            views.append(view)
-
-        views = torch.stack(views, dim=0)  # shape: [3, C, H, W]
+        # Load single view
+        view = self._load_svg_as_image(sample['svg_path'])
+        if self.transform:
+            view = self.transform(view)
+        else:
+            view = transforms.ToTensor()(view)
 
         return {
             'occupancy': grid,
-            'views': views,
-            'id': sample_id
+            'view': view,
+            'view_type': sample['view_type'],
+            'id': sample['base_name']
         }
 
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
+## ----------- Example Use ----------- ##
 
-view_transform = transforms.Compose([
-    transforms.Resize((128, 128)),
-    transforms.ToTensor()
-])
+# from torch.utils.data import DataLoader
+# import torchvision.transforms as transforms
 
-dataset = OccupancyGridDataset("/home/mmpug/Desktop/CADSTUFF/L3DPROJ/L3D_project_2D_to_3D/dataset/filtered", transform=view_transform)
-dataloader = DataLoader(dataset, batch_size=2)
+# view_transform = transforms.Compose([
+#     transforms.Resize((128, 128)),
+#     transforms.ToTensor()
+# ])
 
-for batch in dataloader:
-    print("BATCH ID : ", batch['id'])                         # list of sample names
-    print("OCCUPANCY : ", batch['occupancy'].shape)            # e.g. [B, D, H, W]
-    print("VIEWS SHAPE : ", batch['views'].shape)                # e.g. [B, 3, 3, 128, 128]
+# dataset = OccupancyGridDataset(
+#     occupancy_dir='/home/mmpug/Desktop/CADSTUFF/L3DPROJ/L3D_project_2D_to_3D/dataset/filtered/occupancy',
+#     svg_dir='/home/mmpug/Desktop/CADSTUFF/L3DPROJ/L3D_project_2D_to_3D/dataset/filtered/svgs',
+#     transform=transforms.Compose([
+#         transforms.Resize((128, 128)),
+#         transforms.ToTensor()
+#     ])
+# )
 
 
-## Filtered dataset creation
+# dataloader = DataLoader(dataset, batch_size=2)
+# for batch in dataloader:
+#     print("BATCH ID : ", batch['id'])                         # list of sample names
+#     print("OCCUPANCY : ", batch['occupancy'].shape)            # e.g. [B, D, H, W]
+#     print("VIEWS SHAPE : ", batch['view'].shape)                # e.g. [B, 3, 128, 128]
+#     print("VIEW TYPE : ", batch['view_type'])
+
+## ----------- END Example Use ----------- ##
+
+## ----------- Filtered dataset creation ----------- ##
 # import os
 # import shutil
 
@@ -126,3 +152,5 @@ for batch in dataloader:
 #         count += 1
 
 # print(f" Copied {count} valid samples with all 3 views.")
+
+## ----------- END Filtered dataset creation ----------- ##
